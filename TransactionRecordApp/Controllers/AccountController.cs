58b559a57eb2f2ebfa146a8510d276301b67b240
snickk95 <51108751+snickk95@users.noreply.github.com>
2024-03-27 18:55:29 -0400

    set u p account controller

---
+++ TransactionRecordApp/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TransactionRecordApp.Models;
+
+namespace TransactionRecordApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        //private variables for signing manager and user manager
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+
+        //constructor to set up the private sign in and user managers
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
