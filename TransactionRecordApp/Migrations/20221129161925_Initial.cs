9d9688ee18ee0d7bd66dad03ff6f1b6275350e92
snickk95 <51108751+snickk95@users.noreply.github.com>
2024-03-27 18:25:04 -0400

    Add project files.

---
+++ TransactionRecordApp/Migrations/20221129161925_Initial.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TransactionRecordApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransactionType",
+                columns: table => new
+                {
+                    TransactionTypeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommissionFee = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionType", x => x.TransactionTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TickerSymbol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    SharePrice = table.Column<double>(type: "float", nullable: false),
+                    TransactionTypeId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                    table.ForeignKey(
+                        name: "FK_Transactions_TransactionType_TransactionTypeId",
+                        column: x => x.TransactionTypeId,
+                        principalTable: "TransactionType",
+                        principalColumn: "TransactionTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TransactionType",
+                columns: new[] { "TransactionTypeId", "CommissionFee", "Name" },
+                values: new object[,]
+                {
+                    { "B", 5.4000000000000004, "Buy" },
+                    { "S", 5.9900000000000002, "Sell" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Transactions",
+                columns: new[] { "TransactionId", "CompanyName", "Quantity", "SharePrice", "TickerSymbol", "TransactionTypeId" },
+                values: new object[,]
+                {
+                    { 1, "Apple", 2, 142.90000000000001, "NASDAQ:AAPL", "B" },
+                    { 2, "Ford Motors Company", 4, 12.82, "NYSE:F", "S" },
+                    { 3, "Alphabet Inc.", 100, 2701.7600000000002, "NASDAQ:GOOG", "S" },
+                    { 4, "Microsoft Corporation", 100, 123.45, "NASDAQ:MSFT", "B" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TransactionTypeId",
+                table: "Transactions",
+                column: "TransactionTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "TransactionType");
+        }
+    }
+}
